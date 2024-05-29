15fc99c9f0cb87218a726298300cdc8e44c31d9c
Martin Stubian <mstubian223@gmail.com>
2024-05-29 13:51:09 +0200

    Improvements
    Signed-off-by: Martin Stubian <mstubian223@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap_set = vim.keymap.set
+vim.keymap.set = function(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  return keymap_set(mode, lhs, rhs, opts)
+end
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "wO", "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>")
+vim.keymap.set("n", "wo", "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>")
+
+vim.keymap.set("n", "gh", vim.lsp.buf.hover)
+vim.keymap.set("n", "J", "mzJ`z`")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,37 +1,40 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    settings = {
-      save_on_toggle = true,
-      sync_on_ui_close = true,
-      key = function()
-        return vim.loop.cwd()
-      end,
-    },
-    config = function()
-      local harpoon = require("harpoon")
-      harpoon:setup()
-      vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():append()
-      end)
-      vim.keymap.set("n", "<leader>hm", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
-
-      for i = 1, 10, 1 do
-        vim.keymap.set("n", string.format("<leader>%s", i), function()
-          harpoon:list():select(i)
-        end)
-      end
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<C-S-P>", function()
-        harpoon:list():prev()
-      end)
-      vim.keymap.set("n", "<C-S-N>", function()
-        harpoon:list():next()
-      end)
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  settings = {
+    save_on_toggle = true,
+    sync_on_ui_close = true,
+    key = function()
+      return vim.loop.cwd()
     end,
   },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+
+    vim.keymap.set("n", "<leader>hm", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end)
+
+    for i = 1, 10, 1 do
+      vim.keymap.set("n", string.format("<leader>%s", i), function()
+        harpoon:list():select(i)
+      end)
+    end
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-S-P>", function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set("n", "<C-S-N>", function()
+      harpoon:list():next()
+    end)
+  end,
 }
