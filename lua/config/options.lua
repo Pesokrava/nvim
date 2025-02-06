4223e2362a4ac612681f1890fce0cdb2d0f3ddd0
Martin Stubian <mstubian223@gmail.com>
2025-02-06 14:04:07 +0100

    OSC yanking + black formatter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,14 @@ vim.opt.nu = true
 vim.g.autoformat = false
 vim.opt.clipboard = ""
 vim.o.autoread = true
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
